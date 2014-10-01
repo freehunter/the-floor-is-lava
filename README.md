984ee1f4bef4d0b7e071ae63503f8b97b4954bf6
Mark Hunt <thefreehunter@gmail.com>
2014-10-01 13:29:41 -0500

    added version numbers
    added version numbers and cleaned up old comments in the code

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 the-floor-is-lava
 =================
 
+v0.5
+
 The floor is lava. Don't touch it!
 
-Very early stages, infinite running side scroller.
-Jump from platform to platform as they collpase under you.
+Infinite running side-scroller.
+Jump from platform to platform as they collapse under you.
 
 Space, click, or tap to jump
 Every time you touch a platform, you can double jump
 
-Needs new art assets and sound
+TODO:
+Replace platform models with solid colors
+Make original sprite for player
+Add basic sounds
+Show highest score above starting platform when beginning game
--- main.js
+++ main.js
@@ -1,27 +1,6 @@
-// Initialize Phaser, and creates a 400x490px game
-//var game = new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv');
-
-/*
-//hack to work on multiple screen sizes... i didnt have much luck
-var w = window.innerWidth * window.devicePixelRatio,
-h = window.innerHeight * window.devicePixelRatio,
-width = (h > w) ? h : w,
-height = (h > w) ? w : h;
-
-// Hack to avoid iPad Retina and large Android devices. Tell it to scale up.
-if (window.innerWidth >= 1024 && window.devicePixelRatio >= 2)
-    {
-        width = Math.round(width / 2);
-        height = Math.round(height / 2);
-    }
-
-// reduce screen size by one 3rd on devices like Nexus 5
-if (window.devicePixelRatio === 3)
-    {
-        width = Math.round(width / 3) * 2;
-        height = Math.round(height / 3) * 2;
-    }
-*/
+//The Floor is Lava
+//Platform jumping game
+//v0.5
 
 var game = new Phaser.Game(400, 490, Phaser.CANVAS, 'gameDiv');
 
